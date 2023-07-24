7ff7e9fae57f855cdada34adc451aac5fc65c7dc
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-24 22:32:48 +0300

    commit

--- src/api/controllers/client-api/image/upload.js
+++ src/api/controllers/client-api/image/upload.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     try {
         let img = req.body.img;
         const image = new Image({
-            data: JSON.stringify(img),
+            data: img,
         })
         await image.save().catch((err) => {
             console.log(err);
