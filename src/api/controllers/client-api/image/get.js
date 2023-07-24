646259d303c8639aa6328ccaa940f3bd0cc24cd4
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-24 14:08:20 +0300

    commit

--- src/api/controllers/client-api/image/get.js
+++ src/api/controllers/client-api/image/get.js
@@ -1,14 +1,18 @@
 import { Image } from '../../../../models/index.js';
 
 export default async (req, res) => {
-    let skip = req.body.skip;
     let status = true;
-
-    const list = await Image.find().skip(skip).limit(5).catch((err) => {
-        console.log(err);
+    let list = [];
+    try {
+        let skip = req.body.skip;
+        list = await Image.find().skip(skip).limit(5).catch((err) => {
+            console.log(err);
+            status = false;
+        });
+    } catch (error) {
+        console.log(error);
         status = false;
-    });
-
+    }
     return res.status(200).json({
         resultMessage: { en: getText('en', '00089'), tr: getText('tr', '00089') },
         resultCode: '00089',
--- src/api/controllers/client-api/image/upload.js
+++ src/api/controllers/client-api/image/upload.js
@@ -1,17 +1,21 @@
 import { Image } from '../../../../models/index.js';
 
 export default async (req, res) => {
-    let img = req.body.img;
     let status = true;
+    try {
+        let img = req.body.img;
+        const image = new Image({
+            data: JSON.stringify(img),
+        })
+        await image.save().catch((err) => {
+            console.log(err);
+            status = false;
+        })
 
-    const image = new Image({
-        data: JSON.stringify(img),
-    })
-    await image.save().catch((err) => {
-        console.log(err);
+    } catch (error) {
         status = false;
-    })
-
+        console.log(error);
+    }
     return res.status(200).json({
         resultMessage: { en: getText('en', '00089'), tr: getText('tr', '00089') },
         resultCode: '00089',
