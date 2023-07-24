560f3e89101395560317d81df86e5452e490e2f5
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-24 22:10:20 +0300

    commit

--- src/api/controllers/client-api/image/get.js
+++ src/api/controllers/client-api/image/get.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     let list = [];
     try {
         let skip = req.body.skip;
-        list = await Image.find().skip(skip).limit(5).catch((err) => {
+        list = await Image.findOne().sort({ createdAt: -1 }).skip(skip).catch((err) => {
             console.log(err);
             status = false;
         });
@@ -14,8 +14,6 @@ export default async (req, res) => {
         status = false;
     }
     return res.status(200).json({
-        resultMessage: { en: getText('en', '00089'), tr: getText('tr', '00089') },
-        resultCode: '00089',
         status: status,
         img: list
     });
