9319d9ae7aa81a7dd937e6e7e9c1539aaec7c9ac
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-27 17:56:50 +0300

    commit

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -37,7 +37,7 @@ export default (app) => {
 
   app.use(rateLimiter);
   // app.use(prefix, routes);
-  const cache = new NodeCache({ stdTTL: 15 });
+  const cache = new NodeCache({ stdTTL: 15, deleteOnExpire: false });
 
   const verifyCache = (req, res, next) => {
     try {
@@ -76,6 +76,29 @@ export default (app) => {
     res.send({
       status: status,
       img: list
+    });
+    res.end();
+    return;
+  });
+
+  app.post("/api/image/upload", verifyCache, async (req, res) => {
+    let status = true;
+    try {
+      let img = req.body.img;
+      const image = new Image({
+        data: img,
+      })
+      await image.save().catch((err) => {
+        console.log(err);
+        status = false;
+      })
+
+    } catch (error) {
+      status = false;
+      console.log(error);
+    }
+    res.send({
+      status: status,
     });
     res.end();
     return;
