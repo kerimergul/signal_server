dc96e1866e6ebeed57d48e6f9a2a39dce590dfc1
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2023-07-27 16:04:01 +0300

    commit

--- src/api/controllers/client-api/image/get.js
+++ src/api/controllers/client-api/image/get.js
@@ -5,15 +5,19 @@ export default async (req, res) => {
     let list = [];
     try {
         let skip = req.body.skip;
+        console.time('countOf')
         const countOf = await Image.countDocuments();
         if (skip >= countOf) {
             skip = 0;
         }
+        console.timeEnd('countOf')
 
+        console.time('image')
         list = await Image.findOne().sort({ createdAt: -1 }).skip(skip).catch((err) => {
             console.log(err);
             status = false;
         });
+        console.timeEnd('image')
     } catch (error) {
         console.log(error);
         status = false;
